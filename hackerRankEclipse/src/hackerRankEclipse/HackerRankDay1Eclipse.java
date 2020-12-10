2bca4c1a5babdff5f319f76d89ed006dc23482cb
dieflores <60197427+dieflores@users.noreply.github.com>
2020-12-10 15:13:17 -0300

    primer commit desde eclipse a Git

---
+++ hackerRankEclipse/src/hackerRankEclipse/HackerRankDay1Eclipse.java
@@ -0,0 +1,68 @@
+package hackerRankEclipse;
+
+import java.util.Scanner;
+
+public class HackerRankDay1Eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        
+/*	ESTO VENIA EN EL PROBLEMA	
+ 
+  
+        import java.io.*;
+		import java.util.*;
+		import java.text.*;
+		import java.math.*;
+		import java.util.regex.*;
+
+		public class Solution {
+			
+		    public static void main(String[] args) {
+		        int i = 4;
+		        double d = 4.0;
+		        String s = "HackerRank ";
+				
+		        Scanner scan = new Scanner(System.in);
+
+		        /* Declare second integer, double, and String variables. */
+
+		        /* Read and save an integer, double, and String to your variables.*/
+		        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+		        
+		        /* Print the sum of both integer variables on a new line. */
+
+		        /* Print the sum of the double variables on a new line. */
+				
+		        /* Concatenate and print the String variables on a new line; 
+		        	the 's' variable above should be printed first. */
+
+		// ESTO LO AGREGUE YO Y DE DOS MANERAS 
+		
+		// Scanner scn = new Scanner(System.in);  
+
+		int a = i + 12; //scn.nextInt();
+		double b = 4.0 + d ; // scn.nextDouble();
+		 // String str = "HackerRank";
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(s +""+"is the best place to learn and practice coding!" );
+
+
+
+
+		        scan.close();
+		    }
+		
+
+	}
+
+
---
+++ hackerRankEclipse/src/module-info.java
@@ -0,0 +1,2 @@
+module hackerRankEclipse {
+}
